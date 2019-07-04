43cb7a6591570b6a28cf6091d651e8fc72e8142b
Fekadu <fekaduw@gmail.com>
2019-07-04 11:21:40 -0500

    inheritance assignment updated

--- js/jsinheritance.js
+++ js/jsinheritance.js
@@ -1,36 +1,45 @@
-var createBicyclePrototye = (function() {
+"use strict";
+
+var Module = (function() {
+  var createBicyclePrototye = (function() {
+    return {
+      speed: 0,
+      applyBreak: function(value) {
+        return parseInt(this.speed - value);
+      },
+      speedUp: function(value) {
+        return parseInt(this.speed + value);
+      }
+    };
+  })();
+
+  var createMountainBikeProtoype = function(protoParam) {
+    var newObj = Object.create(protoParam);
+    newObj.gear = 1;
+    newObj.setGear = function(gearValue) {
+      newObj.gear = gearValue;
+    };
+
+    return newObj;
+  };
+
+  var start = function() {
+    var bicyclePrototype = Object.create(createBicyclePrototye);
+    var mountainBikePrototype = Object.create(
+      createMountainBikeProtoype(bicyclePrototype)
+    );
+
+    console.log(bicyclePrototype.speed);
+    console.log(bicyclePrototype.speedUp(20));
+
+    mountainBikePrototype.setGear(5);
+    console.log(mountainBikePrototype.gear);
+  };
   return {
-    speed: 0,
-    applyBreak: function(value) {
-      return parseInt(this.speed - value);
-    },
-    speedUp: function(value) {
-      return parseInt(this.speed + value);
-    }
+    createBicyclePrototype: createBicyclePrototye,
+    createMountainBikeProtoype: createMountainBikeProtoype,
+    start: start
   };
 })();
 
-var createMountainBikeProtoype = function(protoParam) {
-  var newObj = Object.create(protoParam);
-  newObj.gear = 1;
-  newObj.setGear = function(gearValue) {
-    newObj.gear = gearValue;
-  };
-
-  return newObj;
-};
-
-var start = function() {
-  var bicyclePrototype = Object.create(createBicyclePrototye);
-  var mountainBikePrototype = Object.create(
-    createMountainBikeProtoype(bicyclePrototype)
-  );
-
-  console.log(bicyclePrototype.speed);
-  console.log(bicyclePrototype.speedUp(20));
-
-  mountainBikePrototype.setGear(5);
-  console.log(mountainBikePrototype.gear);
-};
-
-start();
+Module.start();
